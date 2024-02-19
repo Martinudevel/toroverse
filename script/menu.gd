976cfa00df14b71807ede2e81a6abc6587f1e58e
Your Name <you@example.com>
2024-02-19 16:49:23 +0200

    + Character creation

--- script/menu.gd
+++ script/menu.gd
@@ -4,3 +4,13 @@ extends Node2D
 
 func _on_quit_button_down():
 	get_tree().quit() 
+
+
+func _on_option_button_down():
+	$optionmenu.visible=true
+
+
+
+func _on_close_button_down():
+	$optionmenu.visible=false
+	
