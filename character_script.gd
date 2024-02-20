f32dfbc455a07f9f79b2290018a6d3b5bea77cdb
Rilotro <136201848+Rilotro@users.noreply.github.com>
2024-02-20 14:05:53 +0200

    Player Character movement
    It's procedural, baby!

---
+++ character_script.gd
@@ -0,0 +1,51 @@
+extends Node2D
+@export var f = 0.5#determines how fast the object arrives at destination and the frequency of vibrations (if there are any)
+@export var zeta = 1#dampens vibrations over time (if at zero, it never stops vibrating), if >= 1 the body never vibrates and instead slows it down
+@export var r = 1#determines the time it takes for the object to start moving if <0 the object anticipates, if >0 begins immediatly, if >1 it overshoots (then corrects, of course)
+
+var k1 = 0;
+var k2 = 0;
+var k3 = 0;
+
+@export var speed = 1#the speed of the object
+
+var y = Vector2(0, 0)
+var yd = Vector2(0, 0)
+
+var x = Vector2(0, 0)
+var xd = Vector2(0, 0)
+var xp = Vector2(0, 0)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	y = self.position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	k1 = zeta/(PI*f)
+	k2 = 1/pow((2*PI*f), 2)
+	k3 = r*zeta/(2*PI*f)
+	
+	if(Input.is_action_pressed("Move_Down")):
+		print("S");
+		#self.position = Vector2(self.position.x, self.position.y+1)
+		x = Vector2(x.x, x.y+speed)
+	if(Input.is_action_pressed("Move_Up")):
+		print("W");
+		#self.position = Vector2(self.position.x, self.position.y-1)
+		x = Vector2(x.x, x.y-speed)
+	if(Input.is_action_pressed("Move_Right")):
+		print("D");
+		#self.position = Vector2(self.position.x+1, self.position.y)
+		x = Vector2(x.x+speed, x.y)
+	if(Input.is_action_pressed("Move_Left")):
+		print("A");
+		#self.position = Vector2(self.position.x-1, self.position.y)
+		x = Vector2(x.x-speed, x.y)
+	if(xd == Vector2(0, 0)):
+		xd = x-xp/delta
+		xp = x
+	y = y + delta*yd
+	yd = yd + delta*(x + k3*xd - y - k1*yd)/k2
+	
+	self.position = y
