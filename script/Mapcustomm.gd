0c1746347a6038dd63220a925467936fb9657a00
Your Name <you@example.com>
2024-02-20 23:13:20 +0200

    rewriting the entire customization to be compatible with saveon JSON

--- script/Mapcustomm.gd
+++ script/Mapcustomm.gd
@@ -1,44 +1,38 @@
 extends Node2D
 var Cname="Player"
 var y=100
-var x=60
+var x=155
 @export var a=0
 var pressed=false
-
-
+var h1=false
+var h2=false
+var h3=false
+var hair
 
 func _ready():
+	pass
 	load_a()
+	load_d()
+	load_d2()
 	
 func _on_add_button_down():
 	$Character/Character_add.visible=true
 	if !pressed:
 		var character_save=load("res://sceene/cbaracter_save.tscn")
 		var character_c=character_save.instantiate()
-		$Character/Save.add_child(character_c)
+		$Character/Save/state.add_child(character_c)
 		character_c.name="Player"+str(a)
 		character_c.position=Vector2(0,y)
-		var hair1=Sprite2D.new()
-		var hair2=Sprite2D.new()
-		var hair3=Sprite2D.new()
-		var z="Character/Save/"+"Player"+str(a)
-		var newmode=get_node(z)
-		newmode.add_child(hair1)
-		hair1.name="hair1"
-		hair1.texture=load("res://graphics/players + entities/playercustomization/fff&text=head1.png")
-		hair1.position=Vector2(60,x)
-		hair1.scale=Vector2(4,4)
-		newmode.add_child(hair2)
-		hair2.name="hair2"
-		hair2.texture=load("res://graphics/players + entities/playercustomization/fff&text=head2.png")
-		hair2.position=Vector2(60,x)
-		hair2.scale=Vector2(4,4)
-		newmode.add_child(hair3)
-		hair3.name="hair3"
-		hair3.texture=load("res://graphics/players + entities/playercustomization/fff&text=head3.png")
-		hair3.position=Vector2(60,x)
-		hair3.scale=Vector2(4,4)
+		 
+		hair=Sprite2D.new()
+		var newmode=$Character/Save/hair
+		hair.name="hair"+str(a)
+		hair.texture=load("res://graphics/players + entities/playercustomization/fff&text=head1.png")
+		hair.position=Vector2(60,x)
+		hair.scale=Vector2(4,4)
+		newmode.add_child(hair)
 		y+=120
+		x+=120
 		pressed=true
 func _on_head_button_down():
 	#player
@@ -46,45 +40,27 @@ func _on_head_button_down():
 	$Character/Character_add/CharacterBody2D/hair/head2.visible=false
 	$Character/Character_add/CharacterBody2D/hair/head3.visible=false
 	#save
-	var z="Character/Save/"+"Player"+str(a)+"/hair1"
-	var p="Character/Save/"+"Player"+str(a)+"/hair2"
-	var q="Character/Save/"+"Player"+str(a)+"/hair3"
-	var hair1=get_node(z)
-	var hair2=get_node(p)
-	var hair3=get_node(q)
-	hair1.visible=true
-	hair2.visible=false
-	hair3.visible=false
+	h1=true
+	hair.texture=load("res://graphics/players + entities/playercustomization/fff&text=head1.png")
+	return h1
 func _on_head_2_button_down():
 	#player
 	$Character/Character_add/CharacterBody2D/hair/head1.visible=false
 	$Character/Character_add/CharacterBody2D/hair/head2.visible=true
 	$Character/Character_add/CharacterBody2D/hair/head3.visible=false
 	#save
-	var z="Character/Save/"+"Player"+str(a)+"/hair1"
-	var p="Character/Save/"+"Player"+str(a)+"/hair2"
-	var q="Character/Save/"+"Player"+str(a)+"/hair3"
-	var hair1=get_node(z)
-	var hair2=get_node(p)
-	var hair3=get_node(q)
-	hair2.visible=true
-	hair1.visible=false
-	hair3.visible=false
+	hair.texture=load("res://graphics/players + entities/playercustomization/fff&text=head2.png")
+	h2=true
+	return h2
 func _on_head_3_button_down():
 	#player
 	$Character/Character_add/CharacterBody2D/hair/head1.visible=false
 	$Character/Character_add/CharacterBody2D/hair/head2.visible=false
 	$Character/Character_add/CharacterBody2D/hair/head3.visible=true
 	#save
-	var z="Character/Save/"+"Player"+str(a)+"/hair1"
-	var p="Character/Save/"+"Player"+str(a)+"/hair2"
-	var q="Character/Save/"+"Player"+str(a)+"/hair3"
-	var hair1=get_node(z)
-	var hair2=get_node(p)
-	var hair3=get_node(q)
-	hair3.visible=true
-	hair2.visible=false
-	hair1.visible=false
+	hair.texture=load("res://graphics/players + entities/playercustomization/fff&text=head3.png")
+	h3=true
+	return h3
 func _on_bodyf_button_down():
 	print(a)
 
@@ -97,6 +73,7 @@ func _on_button_button_down():
 	pressed=false
 	a+=1
 	save_state_a()
+	save_state_d()
 
 func save_c():
 	var save_c={
@@ -110,14 +87,20 @@ func save_c():
 	
 func save_d():
 	var save_d={
-	"filename": get_scene_file_path(),
-	"parent":get_parent().get_path(),
-	"hair1":$hair1.visible,
-	"hair2":$hair2.visible,
-	"hair3":$hair3.visible,
+	"filename":"res://sceene/cbaracter_save.tscn",
+	"parent":"Character/Save/",
+	"z":"Character/Save/"+"Player"+str(a)+"/hair1",
+	"p":"Character/Save/"+"Player"+str(a)+"/hair2",
+	"q":"Character/Save/"+"Player"+str(a)+"/hair3",
+	"hair1":h1,
+	"hair2":h2,
+	"hair3":h3,
+	"hp":"Character/Save/",
 	"position_x":0,
-	"position_y":y
+	"position_y":y,
+	"pos_y_h":x
 	}
+	print(save_d)
 	return save_d
 func save_a():
 	var save_a={
@@ -144,9 +127,79 @@ func load_a():
 	var var_data=json.get_data()
 	self.set("a",var_data)
 	
+func save_state_d():
+	var save_file=FileAccess.open("res://save/save_d.save",FileAccess.WRITE)
+	var vardata=self.call("save_d")
+	var json_string=JSON.stringify(vardata)
+	save_file.store_line(json_string)
+	
+func load_d():
+	if not FileAccess.file_exists("res://save/save_d.save"):
+		return
+	var save_at=FileAccess.open("res://save/save_d.save",FileAccess.READ)
+	var json_string=save_at.get_line()
+	var json=JSON.new()
+	var praseresult=json.parse(json_string)
+	if not praseresult==OK:
+		print("JSON Parase Error:",json.get_error_message(),"in",json_string,"at line",json.get_error_line())
+
+	var node_data=json.get_data()
+	var new_object=load(node_data["filename"]).instantiate()
+	get_node(node_data["parent"]).add_child(new_object)
+	new_object.name="Player"+str(a)
+	new_object.position=Vector2(node_data["position_x"],node_data["position_y"])
+	
+	for i in node_data.keys():
+		if i=="filename" or i=="parent" or i=="position_x" or i=="position_y" :
+			continue
+		new_object.set(i,node_data[i])
+	
+func load_d2():
+	if not FileAccess.file_exists("res://save/save_d.save"):
+		return
+	var save_at=FileAccess.open("res://save/save_d.save",FileAccess.READ)
+	var json_string=save_at.get_line()
+	var json=JSON.new()
+	var praseresult=json.parse(json_string)
+	if not praseresult==OK:
+		print("JSON Parase Error:",json.get_error_message(),"in",json_string,"at line",json.get_error_line())
+
+	var node_data=json.get_data()
+	var h=Sprite2D.new()
+	get_node(node_data["hp"]).add_child(h)
+	h.position=Vector2(60,node_data["pos_y_h"])
+	h.scale=Vector2(4,4)
+	if node_data["hair1"]==true: 
+		h.texture=load("res://graphics/players + entities/playercustomization/fff&text=head1.png")
+	if node_data["hair2"]==true:
+		h.texture=load("res://graphics/players + entities/playercustomization/fff&text=head2.png")
+	if node_data["hair3"]==true:
+		h.texture=load("res://graphics/players + entities/playercustomization/fff&text=head3.png")
+	
+	for i in node_data.keys():
+		if i=="hp" or i=="pos_y_h":
+			continue
+		h.set(i,node_data[i])
 	
 	
+func save_state_c():
+	var save_file=FileAccess.open("res://save/save_c.save",FileAccess.WRITE)
+	var vardata=self.call("save_c")
+	var json_string=JSON.stringify(vardata)
+	save_file.store_line(json_string)
 	
+func load_c():
+	if not FileAccess.file_exists("res://save/save_a.save"):
+		return
+	var save_at=FileAccess.open("res://save/save_a.save",FileAccess.READ)
+	var json_string=save_at.get_line()
+	var json=JSON.new()
+	var praseresult=json.parse(json_string)
+	if not praseresult==OK:
+		print("JSON Parase Error:",json.get_error_message(),"in",json_string,"at line",json.get_error_line())
+
+	var var_data=json.get_data()
+	self.set("a",var_data)
 	
 	
 #var node_to_save=$Character/Character_add/CharacterBody2D
