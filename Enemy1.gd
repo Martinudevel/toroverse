06a7d13f3e7c2725565ecba2f203de64b737a55f
Rilotro <136201848+Rilotro@users.noreply.github.com>
2024-03-19 21:23:12 +0200

    last push
    Added enemy attacks

--- Enemy1.gd
+++ Enemy1.gd
@@ -3,16 +3,13 @@ extends Node2D
 var Player: CharacterBody2D = null
 var speed: int = 3
 @onready var range = $Enemy_Detection/CollisionShape2D.shape.radius
-var range_of_attack: int = 5
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var range_of_attack: int = 50
+var alive: bool = true
+@onready var bludgeon_texture: Texture2D = preload("res://inventory/sprites/bludgeon_sprite.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Player != null):
+	if((Player != null)&&(alive == true)):
 		var new_pos = self.position
 		if(self.position.x < Player.position.x):
 			new_pos.x += speed
@@ -25,12 +22,22 @@ func _process(delta):
 			new_pos.y -= speed
 		
 		if(sqrt(pow((new_pos.x - Player.position.x), 2) + pow((new_pos.y - Player.position.y), 2)) <= range_of_attack):
-			print("attack")
+			attack_player()
 		else:
-			print(new_pos)
 			self.position = new_pos
 
+func take_damage(damage: int, tool: String):
+	if((tool == "sword")||(tool == "endSword")):
+		$Health_Bar.take_damage(damage);
 
 func _on_enemy_detection_body_entered(body):
 	if(body.has_method("use_tool")):
 		Player = body
+
+func death():
+	alive = false
+	await get_tree().create_timer(0.5).timeout
+	self.queue_free()
+
+func attack_player():
+	$Bludgeon.use("bludgeon", bludgeon_texture, Player.global_position)
